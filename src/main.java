6a92350d4bcf7bd8d1b6787623bd9d0e4e20a5ac
Scout Jarman <scoutjarman@aggiemail.usu.edu>
2019-10-01 21:08:42 -0600

    Just some slight changes to the main function so I can try out changing my git commit editor to be **NANO!!!**, which is pretty dope.

--- src/main.java
+++ src/main.java
@@ -4,6 +4,12 @@ public class main {
 		for(int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
+		System.out.println("More code for testing stuff");
+		int a = 5;
+		int b = 6;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("a+b=" + (a+b));
 	}
 
 }
